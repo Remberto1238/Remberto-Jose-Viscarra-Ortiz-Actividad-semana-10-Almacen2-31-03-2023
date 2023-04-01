2472d82a88d0f8a8abbaafa7017c42eba675caa5
Remberto1238 <125625467+Remberto1238@users.noreply.github.com>
2023-03-31 19:59:49 -0600

    Add files via upload

---
+++ Almacen2/DAO/CrudProducto.cs
@@ -0,0 +1,100 @@
+﻿using Almacen2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen2.DAO
+{
+    public class CrudProductos
+    {
+        public void AgregarUsuario(Producto ParamentrosProducto)
+        {
+
+            using (Almacen2Context db =
+                new Almacen2Context())
+            {
+                Producto producto = new Producto();
+                producto.Nombre = ParamentrosProducto.Nombre;
+                producto.Precio = ParamentrosProducto.Precio;
+                producto.Descripcion = ParamentrosProducto.Descripcion;
+                producto.Stock = ParamentrosProducto.Stock;
+                db.Add(producto);
+                db.SaveChanges();
+            }
+        }
+
+        public Producto UsuarioIndividual(int id)
+        {
+            using (Almacen2Context bd = new Almacen2Context())
+            {
+
+                var buscar = bd.Productos.FirstOrDefault(x => x.Id == id);
+
+                return buscar;
+            }
+        }
+        public void ActualizarUsuario(Producto ParamentrosUsuario, int Lector)
+        {
+            using (Almacen2Context db =
+                new Almacen2Context())
+            {
+
+                var buscar = UsuarioIndividual(ParamentrosUsuario.Id);
+                if (buscar == null)
+                {
+                    Console.WriteLine("El id no existe");
+                }
+                else
+                {
+                    if (Lector == 1)
+                    {
+                        buscar.Nombre = ParamentrosUsuario.Nombre;
+                        buscar.Descripcion = ParamentrosUsuario.Descripcion;
+                    }
+                    else
+                    {
+                        buscar.Precio = ParamentrosUsuario.Precio;
+                        buscar.Stock = ParamentrosUsuario.Stock;
+                    }
+
+                    buscar.Descripcion = ParamentrosUsuario.Descripcion;
+                    db.Update(buscar);
+                    db.SaveChanges();
+
+                }
+
+            }
+        }
+        public string EliminarUsuario(int id)
+        {
+            using (Almacen2Context db =
+                    new Almacen2Context())
+            {
+                var buscar = UsuarioIndividual(id);
+                if (buscar == null)
+                {
+                    return "El usuario no existe";
+                }
+                else
+                {
+                    db.Productos.Remove(buscar);
+                    db.SaveChanges();
+                    return "El usuario se elimino";
+                }
+            }
+        }
+
+        public List<Producto> ListarProducto()
+        {
+            using (Almacen2Context db =
+                   new Almacen2Context())
+            {
+                return db.Productos.ToList();
+            }
+
+        }
+
+    }
+}
---
+++ Almacen2/Program.cs
@@ -0,0 +1,136 @@
+﻿using Almacen2.DAO;
+using Almacen2.Models;
+
+Console.WriteLine("Precio 1 para Insertar Productos");
+
+Console.WriteLine("Precio 2 para Actualizar Producto");
+
+Console.WriteLine("Precio 3 para Eliminar Producto");
+
+Console.WriteLine("Precio 4 para Mostrar Lista de La Base de Dato");
+
+var menu = Convert.ToInt32(Console.ReadLine());
+
+
+Producto producto = new Producto();
+CrudProductos crudProducto = new CrudProductos();
+
+
+bool Continuar = true;
+while (Continuar)
+    switch (menu)
+{
+    case 1:
+        Console.WriteLine("Ingrese El Nombre del Producto: ");
+        producto.Nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la Descripcion del Producto: ");
+        producto.Descripcion = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el Precio del Producto: ");
+        producto.Precio = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la Cantidad de Stock: ");
+        producto.Stock = Convert.ToInt32(Console.ReadLine());
+
+        crudProducto.AgregarUsuario(producto);
+        
+        Console.WriteLine("Su Registro Ha Sido Guardado Exitosamente...");
+        
+        Console.WriteLine("");
+
+        break;
+
+    case 2:
+        Console.WriteLine("Actualizar datos");
+        Console.WriteLine("");
+        Console.WriteLine("Ingresa el ID del usuario a actualizar");
+        var UsuarioIndividualU = crudProducto.UsuarioIndividual(Convert.ToInt32(Console.ReadLine()));
+        if (UsuarioIndividualU == null)
+        {
+            Console.WriteLine("El Usuario no existe");
+        }
+        else
+        {
+            Console.WriteLine($"Nombre: {UsuarioIndividualU.Nombre}, Descripcion: {UsuarioIndividualU.Descripcion}, Precio: {UsuarioIndividualU.Precio}, Stock: {UsuarioIndividualU.Stock}");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Nombre y Descripcion preciona 1");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Precio y Stock preciona 2");
+
+            var Lector = Convert.ToInt32(Console.ReadLine());
+            if (Lector == 1)
+            {
+                Console.WriteLine("Ingrese el nuevo Nombre: ");
+                UsuarioIndividualU.Nombre = (Console.ReadLine());
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Ingrese la nueva Descripcion: ");
+                UsuarioIndividualU.Descripcion = (Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Ingrese el nuevo Precio: ");
+                UsuarioIndividualU.Precio = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Ingrese el nuevo Stock: ");
+                UsuarioIndividualU.Stock = Convert.ToInt32(Console.ReadLine());
+            }
+            crudProducto.ActualizarUsuario(UsuarioIndividualU, Lector);
+            Console.WriteLine("");
+            Console.WriteLine("Actualizacion Correcta");
+        }
+        break;
+    case 3:
+        Console.WriteLine("Ingresa el Id a Eliminar");
+        var UsuarioIndividualD = crudProducto.UsuarioIndividual(Convert.ToInt32(Console.ReadLine()));
+        if (UsuarioIndividualD == null)
+        {
+            Console.WriteLine("Este Usuario no Existe");
+        }
+        else
+        {
+            Console.WriteLine("Eliminar usuario");
+            Console.WriteLine("");
+            Console.WriteLine($"Nombre: {UsuarioIndividualD.Nombre}, Descripcion: {UsuarioIndividualD.Descripcion}, Precio: {UsuarioIndividualD.Precio}, Stock: {UsuarioIndividualD.Stock},");
+            Console.WriteLine("");
+            Console.WriteLine("El usuario encontrado es el correcto?");
+            var Lector = Convert.ToInt32(Console.ReadLine());
+            if (Lector == 1)
+            {
+                var Id = Convert.ToInt32(UsuarioIndividualD.Id);
+                Console.WriteLine(crudProducto.EliminarUsuario(Id));
+            }
+            else
+            {
+                Console.WriteLine("Inicia Nuevamente");
+            }
+        }
+        break;
+    case 4:
+        Console.WriteLine("Lista de Productos");
+        var ListarProducto = crudProducto.ListarProducto();
+        foreach (var iteracionProducto in ListarProducto)
+        {
+            Console.WriteLine($"{iteracionProducto.Id}, {iteracionProducto.Nombre},  " +
+                $"{iteracionProducto.Descripcion}, {iteracionProducto.Precio}, {iteracionProducto.Stock}");
+
+        }
+        break;
+}
+Console.WriteLine("");
+Console.WriteLine("Desea continuar ?");
+var cont = Console.ReadLine();
+if (cont.Equals("N"))
+{
+    Continuar = false;
+}
+
+}
